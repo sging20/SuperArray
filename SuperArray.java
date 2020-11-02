3c79a4ad78d7b1fa5bd2c9b2d5de6b7d8058b4a8
Sean Ging <sging20@stuy.edu>
2020-11-02 17:28:59 -0500

    Progress Made

---
+++ SuperArray.java
@@ -0,0 +1,16 @@
+public class SuperArray {
+//Instance Variables
+private String[] data;
+private int size; //the current size
+public SuperArray(){
+  size = 10;
+  String[] data= new String[size];
+}
+public int size(){
+  return size;
+
+
+
+
+
+}
